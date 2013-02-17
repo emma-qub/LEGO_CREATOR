6e32b0f04296987fa8189f3d69344596d2d8edb8
vmichele <valentin.michelet@inria.fr>
2013-02-17 09:20:59 +0100

    Correction du calcul de la bounding box de la classe Wheel

--- Wheel.cpp
+++ Wheel.cpp
@@ -1,15 +1,21 @@
 #include "Wheel.h"
 
+#include <QDebug>
+
 Wheel::Wheel(const QColor& color, QObject* parent) :
     Lego(color, parent) {
+
+    calculateBoundingBox();
 }
 
 Wheel::Wheel(const Wheel& wheel) :
     Lego(wheel) {
+
+    calculateBoundingBox();
 }
 
 void Wheel::calculateBoundingBox(void) {
-    _boundingBox = BoundingBox(0, 0, 0, 4, 2, 7);
+    _boundingBox = BoundingBox(0, 0, 0, 4, 2, 5);
 }
 
 Wheel* Wheel::cloning(void) const {
